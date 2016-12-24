bdf1b286ed446812b7e79a3e3d27288e47e996fa
Morgan Quigley <morgan@osrfoundation.org>
2016-12-23 22:31:26 -0800

    create ca cert/key and create and sign governance file

--- sros2.py
+++ sros2.py
@@ -13,11 +13,155 @@
 # limitations under the License.
 
 import argparse
+import os
+import subprocess
 import sys
 
+def create_ca_conf_file(path):
+    with open(path, 'w') as f:
+        f.write("""\
+[ ca ]
+default_ca = CA_default
+
+[ CA_default ]
+dir              = .
+certs            = $dir/certs
+crl_dir          = $dir/crl
+database         = $dir/index.txt
+unique_subject   = no
+new_certs_dir    = $dir
+certificate      = $dir/ca.cert.pem
+private_key      = $dir/ca.key.pem
+serial           = $dir/serial
+crlnumber        = $dir/crlnumber
+crl              = $dir/crl.pem
+private_key      = $dir/ca.key.pem
+RANDFILE         = $dir/private/.rand
+name_opt         = ca_default
+cert_opt         = ca_default
+default_days     = 1825
+default_crl_days = 30
+default_md       = sha256
+preserve         = no
+policy           = policy_match
+
+[ policy_match ]
+countryName = match
+stateOrPrivinceName = match
+organizationName = match
+organizationalUnitName = optional
+commonName = supplied
+emailAddress = optional
+
+[ policy_anything ]
+countryName = optional
+stateOrPrivinceName = optional
+organizationName = optional
+organizationalUnitName = optional
+commonName = supplied
+emailAddress = optional
+
+[ req ]
+prompt = no
+distinguished_name = req_distinguished_name
+string_mask = utf8only
+
+[ req_distinguished_name ]
+commonName = sros2testCA
+
+""")
+
+def run_shell_command(cmd):
+    print("running command: %s" % cmd)
+    subprocess.call(cmd, shell=True)
+
+def create_ecdsa_param_file(path):
+    run_shell_command("openssl ecparam -name prime256v1 > %s" % path)
+
+def create_ca_key_cert(ecdsa_param_path, ca_conf_path, ca_key_path, ca_cert_path):
+    run_shell_command("openssl req -nodes -x509 -days 3650 -newkey ec:%s -keyout %s -out %s -config %s" % (ecdsa_param_path, ca_key_path, ca_cert_path, ca_conf_path))
+
+def create_governance_file(path):
+    # for this application we are only looking to authenticate and encrypt;
+    # we do not need/want access control at this point.
+    with open(path, 'w') as f:
+        f.write("""\
+<?xml version="1.0" encoding="UTF-8"?>
+<dds xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="http://www.omg.org/spec/DDS-SECURITY/20140301/dds_security_governance.xsd">
+    <domain_access_rules>
+        <domain_rule>
+            <domain_id>*</domain_id>
+            <allow_unauthenticated_join>FALSE</allow_unauthenticated_join>
+            <enable_join_access_control>TRUE</enable_join_access_control>
+            <discovery_protection_kind>ENCRYPT</discovery_protection_kind>
+            <liveliness_protection_kind>ENCRYPT</liveliness_protection_kind>
+            <rtps_protection_kind>SIGN</rtps_protection_kind>
+            <topic_access_rules>
+                <topic_rule>
+                    <topic_expression>*</topic_expression>
+                    <enable_discovery_protection>TRUE</enable_discovery_protection>
+                    <enable_read_access_control>FALSE</enable_read_access_control>
+                    <enable_write_access_control>FALSE</enable_write_access_control>
+                    <metadata_protection_kind>ENCRYPT</metadata_protection_kind>
+                    <data_protection_kind>ENCRYPT</data_protection_kind>
+                </topic_rule>
+            </topic_access_rules>
+        </domain_rule>
+    </domain_access_rules>
+</dds>
+""")
+
+def create_signed_governance_file(signed_gov_path, gov_path, ca_cert_path, ca_key_path):
+    run_shell_command("openssl smime -sign -in %s -text -out %s -signer %s -inkey %s" % (gov_path, signed_gov_path, ca_cert_path, ca_key_path))
+
 def create_keystore(args):
-    print("creating keystore")
+    root = args.ROOT
     print(args)
+
+    if not os.path.exists(root):
+        print("creating directory: %s" % root)
+        os.makedirs(root)
+    else:
+        print("directory already exists: %s" % root)
+
+    ca_conf_path = os.path.join(root, 'ca_conf.txt')
+    if not os.path.isfile(ca_conf_path):
+        print("creating CA file: %s" % ca_conf_path)
+        create_ca_conf_file(ca_conf_path)
+    else:
+        print("found CA conf file, not writing a new one!")
+
+    ecdsa_param_path = os.path.join(root, 'ecdsaparam')
+    if not os.path.isfile(ecdsa_param_path):
+        print("creating ECDSA param file: %s" % ecdsa_param_path)
+        create_ecdsa_param_file(ecdsa_param_path)
+    else:
+        print("found ECDSA param file, not writing a new one!")
+
+    ca_key_path = os.path.join(root, 'ca.key.pem')
+    ca_cert_path = os.path.join(root, 'ca.cert.pem')
+    if not (os.path.isfile(ca_key_path) and os.path.isfile(ca_cert_path)):
+        print("creating new CA key/cert pair")
+        create_ca_key_cert(ecdsa_param_path, ca_conf_path, ca_key_path, ca_cert_path)
+    else:
+        print("found CA key and cert, not creating new ones!")
+
+    # create governance file
+    gov_path = os.path.join(root, 'governance.xml')
+    if not os.path.isfile(gov_path):
+        print("creating governance file: %s" % gov_path)
+        create_governance_file(gov_path)
+    else:
+        print("found governance file, not creating a new one!")
+
+    # sign governance file
+    signed_gov_path = os.path.join(root, 'governance.p7s')
+    if not os.path.isfile(signed_gov_path):
+        print("creating signed governance file: %s" % signed_gov_path)
+        create_signed_governance_file(signed_gov_path, gov_path, ca_cert_path, ca_key_path)
+    else:
+        print("found signed governance file, not creating a new one!")
 
 def distribute_keys(args):
     print("distributing keys")
