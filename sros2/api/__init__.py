ccf6fb9fb1251f13193f2ee52943d97eca927001
dhood <dhood@users.noreply.github.com>
2018-02-28 14:52:46 -0800

    Revert "Don't overwrite existing permissions"
    This reverts commit 416f176e28ec455d5c3da0977e50c5264ef53322.

--- sros2/api/__init__.py
+++ sros2/api/__init__.py
@@ -313,10 +313,10 @@ def create_permission_file(path, name, domain_id, permissions_dict):
     if topic_dict:
         # add rules for automatically created ros2 topics
         # TODO(mikaelarguedas) remove this hardcoded handling for default topics
-        if 'parameter_events' not in topic_dict:
-            topic_dict['parameter_events'] = {'allow': 'ps'}
-        if 'clock' not in topic_dict:
-            topic_dict['clock'] = {'allow': 's'}
+        # TODO(mikaelarguedas) update dictionary based on existing rule
+        # if it already exists (rather than overriding the rule)
+        topic_dict['parameter_events'] = {'allow': 'ps'}
+        topic_dict['clock'] = {'allow': 's'}
         # we have some policies to add !
         for topic_name, policy in topic_dict.items():
             tags = []
