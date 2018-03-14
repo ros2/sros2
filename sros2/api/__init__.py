0d4fc19c33adfde5ab9f50092bcf6131ea98e13e
dhood <dhood@users.noreply.github.com>
2018-03-14 09:32:03 -0700

    Linter fixup

--- sros2/api/__init__.py
+++ sros2/api/__init__.py
@@ -341,7 +341,8 @@ def create_permission_file(path, name, domain_id, permissions_dict):
         </%s>
 """ % (tag, 'rt', topic_name, tag)
         # TODO(mikaelarguedas) remove this hardcoded handling for default parameter topics
-        # TODO(mikaelarguedas) remove the need for empty partition (required for Connext at startup),
+        # TODO(mikaelarguedas) remove the need for empty partition
+        # (required for Connext at startup),
         # see https://github.com/ros2/sros2/issues/32#issuecomment-367388140
         service_partitions_prefix = {
             'Request': ['', 'rq/%s' % name],
