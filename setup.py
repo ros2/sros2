3d7cda1a04d0705fcafa69f7db185ea633b65202
Ross Desmond <rddesmon@amazon.com>
2018-11-20 00:02:54 +0000

    Add generate permissions security command line
    Generate an sros2 yaml permissions file with the permissions of every visible node
    on the dds network.
    
    Add custom service security to sros2
    
    Example: run the minimal_publisher_lambda node
    Execute: `ros2 security generate_permissions node_policies.yaml`
    
    It will create the following file in the current directory:
    ```
    /minimal_publisher:
      services:
        /minimal_publisher/describe_parameters:
          allows:
          - request
          - reply
          .
          .
          .
      topics:
        /parameter_events:
          allows:
          - publish
          - subscribe
        /topic:
          allows:
          - publish
    ```
    
    cr https://code.amazon.com/reviews/CR-3943967

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ enhance the security of ROS 2 deployments.""",
             ':CreatePermissionVerb',
             'distribute_key = sros2.verb.distribute_key:DistributeKeyVerb',
             'list_keys = sros2.verb.list_keys:ListKeysVerb',
+            'generate_permissions = sros2.verb.generate_permissions:GeneratePermissionsVerb',
         ],
     }
 )
