2f2c1b87929a513425dc881914c3fca914d43545
ruffsl <roxfoxpox@gmail.com>
2018-12-21 12:56:29 -0800

    Rework example to use sros2 package data

--- examples/profile_to_permissions.py
+++ examples/profile_to_permissions.py
@@ -1,20 +1,34 @@
-import lxml.etree as ET
+from lxml import etree
 
-# Set paths
-profile_xml_path = 'sample_policy.xml'
-profile_xslt_path = 'profile_template.xslt'
-permissions_xslt_path = 'permissions_template.xslt'
+from sros2.policy import (
+    get_policy_schema,
+    get_transport_schema,
+    get_transport_template,
+)
 
-# Load profile
-profile_xml = ET.parse(profile_xml_path)
+# Get paths
+policy_xml_path = 'sample_policy.xml'
+policy_xsd_path = get_policy_schema('policy.xsd')
+policy_xsl_path = get_transport_template('dds', 'policy.xsl')
+permissions_xsl_path = get_transport_template('dds', 'permissions.xsl')
+permissions_xsd_path = get_transport_schema('dds', 'permissions.xsd')
 
-# Load templates
-profile_xslt = ET.XSLT(ET.parse(profile_xslt_path))
-permissions_xslt = ET.XSLT(ET.parse(permissions_xslt_path))
+# Parse files
+policy_xml = etree.parse(policy_xml_path)
+policy_xsd = etree.XMLSchema(etree.parse(policy_xsd_path))
+policy_xsl = etree.XSLT(etree.parse(policy_xsl_path))
+permissions_xsl = etree.XSLT(etree.parse(permissions_xsl_path))
+permissions_xsd = etree.XMLSchema(etree.parse(permissions_xsd_path))
 
-# Apply permission transform
-permissions_xml = profile_xslt(profile_xml)
-permissions_xml = permissions_xslt(permissions_xml)
+# Validate policy schema
+policy_xsd.assertValid(policy_xml)
 
-# Show transform
-print(ET.tostring(permissions_xml, pretty_print=True).decode())
+# Transform policy
+_permissions_xml = policy_xsl(policy_xml)
+permissions_xml = permissions_xsl(_permissions_xml)
+
+# Validate permissions schema
+permissions_xsd.assertValid(permissions_xml)
+
+# Output permissions
+print(etree.tostring(permissions_xml, pretty_print=True).decode())
