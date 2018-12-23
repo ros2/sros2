6442d8abded152cc366db570dbaf9abb85862426
ruffsl <roxfoxpox@gmail.com>
2018-12-23 12:26:15 -0800

    Update example to use multi-pass processing and fix include error

--- examples/policy_to_permissions.py
+++ examples/policy_to_permissions.py
@@ -22,27 +22,24 @@ from sros2.policy import (
 
 # Get paths
 policy_xsd_path = get_policy_schema('policy.xsd')
-policy_xsl_path = get_transport_template('dds', 'policy.xsl')
 permissions_xsl_path = get_transport_template('dds', 'permissions.xsl')
 permissions_xsd_path = get_transport_schema('dds', 'permissions.xsd')
 
 # Parse files
 policy_xsd = etree.XMLSchema(etree.parse(policy_xsd_path))
-policy_xsl = etree.XSLT(etree.parse(policy_xsl_path))
 permissions_xsl = etree.XSLT(etree.parse(permissions_xsl_path))
 permissions_xsd = etree.XMLSchema(etree.parse(permissions_xsd_path))
 
 # Get policy
 policy_xml_path = 'sample_policy.xml'
 policy_xml = etree.parse(policy_xml_path)
-policy_xsd.xinclude()
+policy_xml.xinclude()
 
 # Validate policy schema
 policy_xsd.assertValid(policy_xml)
 
 # Transform policy
-_permissions_xml = policy_xsl(policy_xml)
-permissions_xml = permissions_xsl(_permissions_xml)
+permissions_xml = permissions_xsl(policy_xml)
 
 # Validate permissions schema
 permissions_xsd.assertValid(permissions_xml)
