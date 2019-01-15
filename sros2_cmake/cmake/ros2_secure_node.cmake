ffc49797e20bfc1e75dfa6794ac231059a976b30
Michael Carroll <michael@openrobotics.org>
2019-01-15 15:38:12 -0800

    YAML -> XML

--- sros2_cmake/cmake/ros2_secure_node.cmake
+++ sros2_cmake/cmake/ros2_secure_node.cmake
@@ -18,7 +18,7 @@ macro(ros2_secure_node)
     # NODES (macro multi-arg) takes the node names for which keys will be generated
     # SECURITY (cmake arg) if not define or OFF, will not generate key/keystores
     # ROS_SECURITY_ROOT_DIRECTORY (env variable) will the location of the keystore
-    # POLICY_FILE (cmake arg) if defined, will compile policies by node name into the access private certificates (e.g POLICY_FILE=/etc/policies/<policy.yaml>, Generate: <node_name> /etc/policies/<policy.yaml>)
+    # POLICY_FILE (cmake arg) if defined, will compile policies by node name into the access private certificates (e.g POLICY_FILE=/etc/policies/<policy.xml>, Generate: <node_name> /etc/policies/<policy.xml>)
     IF (NOT SECURITY)
         message(STATUS "Not generating security files")
         return()
