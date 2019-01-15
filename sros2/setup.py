558b12c2ae935ff764d1a0f0a6fa73c64a70d00e
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-01-15 15:41:54 -0800

    0.6.1

--- sros2/setup.py
+++ sros2/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sros2',
-    version='0.5.0',
+    version='0.6.1',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     zip_safe=True,
