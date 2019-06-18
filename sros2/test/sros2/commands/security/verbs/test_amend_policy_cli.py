f642bc4e4b733fa9a99e0e736770b6c7fd1d6e6a
artivis <jeremie.deray@canonical.com>
2019-06-18 11:12:11 -0400

    rm test_amend_policy_no_file & cleanup
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- sros2/test/sros2/commands/security/verbs/test_amend_policy_cli.py
+++ sros2/test/sros2/commands/security/verbs/test_amend_policy_cli.py
@@ -58,31 +58,6 @@ def test_ament_policy():
             node.destroy_node()
             rclpy.shutdown(context=context)
 
-    # # Load the policy and pull out the allowed publications and subscriptions
-    # policy = load_policy(os.path.join(tmpdir, 'test-policy.xml'))
-    # profile = policy.find(path='profiles/profile[@ns="/"][@node="test_node"]')
-    # assert profile is not None
-    # topics_publish_allowed = profile.find(path='topics[@publish="ALLOW"]')
-    # assert topics_publish_allowed is not None
-    # topics_subscribe_allowed = profile.find(path='topics[@subscribe="ALLOW"]')
-    # assert topics_subscribe_allowed is not None
-    #
-    # # Verify that the allowed publications include topic_pub and not topic_sub
-    # topics = topics_publish_allowed.findall('topic')
-    # assert len([t for t in topics if t.text == 'topic_pub']) == 1
-    # assert len([t for t in topics if t.text == 'topic_sub']) == 0
-    #
-    # # Verify that the allowed subscriptions include topic_sub and not topic_pub
-    # topics = topics_subscribe_allowed.findall('topic')
-    # assert len([t for t in topics if t.text == 'topic_sub']) == 1
-    # assert len([t for t in topics if t.text == 'topic_pub']) == 0
-
-
-def test_amend_policy_no_file(capsys):
-    assert cli.main(argv=[
-        'security', 'amend_policy', os.path.join('', 'bar.xml')]) == \
-            'Package policy file not found'
-
 
 def test_amend_policy_file_not_valid(capsys):
     assert cli.main(argv=[
