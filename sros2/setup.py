5ef7de5adc8d8abc12b3cbb58d888473c92b226c
Ruffin <roxfoxpox@gmail.com>
2019-09-20 11:42:11 -0700

    Keep consistent formatting with other index PRs
    Signed-off-by: ruffsl <ruffsl@users.noreply.github.com>
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>

--- sros2/setup.py
+++ sros2/setup.py
@@ -26,7 +26,8 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
