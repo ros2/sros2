58bd4a53f7b6917768fd6f3c75bfaf73dc38bb3a
Mikael Arguedas <mikael.arguedas@gmail.com>
2022-01-11 21:43:38 +0100

    Make use of ros_testing to test policy generation. (#214) (#254)
    * Make use of ros_testing to test policy generation.
    
    Signed-off-by: Brian Ezequiel Marchi <brian.marchi65@gmail.com>
    Co-authored-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Skip test_generate_policy.py tests on Windows.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Revert "Skip test_generate_policy.py tests on Windows."
    
    This reverts commit c68f0a0a68d51c0cb74cdec57f5dfc5dcc0295d3.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Wait for expected ROS graph state.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Boost discovery delay.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Please linter.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Skip tests using RTI Connext w/o a daemon.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Bump generate_policy execution timeout on Windows.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Bump generate_policy execution timeout on Windows again.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    Co-authored-by: Brian Ezequiel Marchi <brian.marchi65@gmail.com>
    
    Co-authored-by: Michel Hidalgo <michel@ekumenlabs.com>
    Co-authored-by: Brian Ezequiel Marchi <brian.marchi65@gmail.com>

---
+++ sros2/test/sros2/commands/security/verbs/test_generate_policy_no_nodes.py
@@ -0,0 +1,65 @@
+# Copyright 2019 Canonical Ltd
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import itertools
+import os
+import sys
+import tempfile
+
+import launch_testing
+from launch_testing.actions import ReadyToTest
+import launch_testing.asserts
+import launch_testing.markers
+import launch_testing.tools
+
+import pytest
+
+from rclpy.utilities import get_available_rmw_implementations
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+from utilities.sros2_cli_test_case import generate_sros2_cli_test_description  # noqa: E402
+from utilities.sros2_cli_test_case import SROS2CLITestCase  # noqa: E402
+
+
+@pytest.mark.rostest
+@launch_testing.parametrize('rmw_implementation,use_daemon', itertools.product(
+    get_available_rmw_implementations(), (True, False)
+))
+@launch_testing.markers.keep_alive
+def generate_test_description(rmw_implementation, use_daemon):
+    return generate_sros2_cli_test_description(
+        fixture_actions=[ReadyToTest()],
+        rmw_implementation=rmw_implementation,
+        use_daemon=use_daemon
+    ), locals()
+
+
+class TestSROS2GeneratePolicyVerbWithNoNodes(SROS2CLITestCase):
+
+    def test_generate_policy_no_nodes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with self.launch_sros2_command(
+                arguments=['generate_policy', os.path.join(tmpdir, 'test-policy.xml')]
+            ) as gen_command:
+                assert gen_command.wait_for_shutdown(timeout=10)
+            assert gen_command.exit_code != launch_testing.asserts.EXIT_OK
+            assert 'No nodes detected in the ROS graph. No policy file was generated.' \
+                in gen_command.output
+
+    def test_generate_policy_no_policy_file(self):
+        with self.launch_sros2_command(arguments=['generate_policy']) as gen_command:
+            assert gen_command.wait_for_shutdown(timeout=2)
+        assert gen_command.exit_code != launch_testing.asserts.EXIT_OK
+        assert 'following arguments are required: POLICY_FILE_PATH' in gen_command.output
