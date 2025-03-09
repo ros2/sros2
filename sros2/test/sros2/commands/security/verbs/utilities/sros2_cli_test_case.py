894d06167fdd9c42d83186df159df7812727b6cb
Mikael Arguedas <mikael.arguedas@gmail.com>
2025-03-09 14:40:17 +0100

    add type hint to rmw_implementation and use_daemon to fix mypy
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- sros2/test/sros2/commands/security/verbs/utilities/sros2_cli_test_case.py
+++ sros2/test/sros2/commands/security/verbs/utilities/sros2_cli_test_case.py
@@ -33,7 +33,7 @@ MAX_DISCOVERY_DELAY = 4.0  # seconds
 
 
 def generate_sros2_cli_test_description(
-    fixture_actions, rmw_implementation, use_daemon
+    fixture_actions, rmw_implementation: str, use_daemon: bool
 ) -> LaunchDescription:
     additional_env = {'RMW_IMPLEMENTATION': rmw_implementation}
     if use_daemon:
